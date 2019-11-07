8d470f1ad7d91d997309b36c4b5cce5ecc74c5eb
hansaka1996 <hansaka.18@itfac.mrt.ac.lk>
2019-11-07 13:29:57 +0530

    test

--- hansaka/hansakarecog.js
+++ hansaka/hansakarecog.js
@@ -1,7 +1,8 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
@@ -14,11 +15,24 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+            let label = data.Labels[0].Name;
+            ddb.put({
+                TableName: 'janaka',
+                Item: {
+                    'Name': s3.object.key,
+                    'label': label
+                }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                });
+
+
+        })
+        .catch(err => callback(err));
 }
-
-
